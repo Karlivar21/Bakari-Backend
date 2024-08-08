a99ea14c2516fd4de4800ebeab7b67fce64b91d9
Karlivar21 <karlk21@ru.is>
2024-08-08 17:42:31 +0000

    bcrypt

--- data/users.js
+++ data/users.js
@@ -1,14 +1,14 @@
 // data/users.js
 const users = [
-    { username: 'kalli', password: 'Karlivar02!' }, // hashed password: password1
-    { username: 'alli', password: '' }, // hashed password: password2
-    { username: 'almar', password: 'kallierbestur'},
-    { username: 'biggi', password: ''},
-    { username: 'bjossi', password: ''},
-    { username: 'kassi1', password: ''},
-    { username: 'kassi2', password: ''},
-    { username: 'heidrun', password: ''},
-    { username: 'sunna', password: ''},
+    { username: 'kalli', password: bcrypt.hashSync('password', 10)}, // hashed password: password1
+    { username: 'alli', password: bcrypt.hashSync('password123', 10)},
+    { username: 'almar', password: bcrypt.hashSync('password123', 10)},
+    { username: 'biggi', password: bcrypt.hashSync('password123', 10)},
+    { username: 'bjossi', password: bcrypt.hashSync('password123', 10)},
+    { username: 'kassi1', password: bcrypt.hashSync('password123', 10)},
+    { username: 'kassi2', password: bcrypt.hashSync('password123', 10)},
+    { username: 'heidrun', password: bcrypt.hashSync('password123', 10)},
+    { username: 'sunna', password: bcrypt.hashSync('password123', 10)}
 ];
 
 export default users;
