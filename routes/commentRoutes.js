6564ba6d83d06bbb067c26850cf135b671a1dbbf
Karlivar21 <karlk21@ru.is>
2024-08-13 21:21:52 +0000

    image

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
     try {
         const { id, name, phone, email, message } = req.body;
-
+        console.log(req.body);  
         if (!name || !email || !message) {
             return res.status(400).json({ message: 'Missing required fields' });
         }
