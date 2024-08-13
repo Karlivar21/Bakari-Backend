3ce31ad959f6833e188f2e89efb7475c9f7e3720
Karlivar21 <karlk21@ru.is>
2024-08-13 20:52:15 +0000

    comment

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const corsOptions = {
   };
 
 app.use(cors(corsOptions));
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json());
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Handle preflight requests
