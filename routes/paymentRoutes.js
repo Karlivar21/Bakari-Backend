e461a495b51b54083cbd7903095c5ee3c2deed3e
Karlivar21 <karlk21@ru.is>
2025-12-14 16:10:30 +0000

    fix

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,7 +1,6 @@
 // routes/paymentRoutes.js
 import express from "express";
-import fetch from "node-fetch"; // if you're on Node 18+, you can remove this and use global fetch
-import Order from "../models/Order.js"; // adjust path/name to your Order model
+import Order from '../models/Order.js';
 
 const router = express.Router();
 
